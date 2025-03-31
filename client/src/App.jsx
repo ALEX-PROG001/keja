825d73b3ba8c68870878f8efc969f3c6c32fb47c
Alex <malexcharis001@gmail.com>
2025-03-31 23:35:40 +0300

    Fix missing @vitejs/plugin-react-swc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import About from './pages/About';
+
 import Profile from './pages/Profile';
 import Header from "./components/Header";
 import BottomNav from './components/BottomNav';
@@ -32,7 +32,7 @@ export default function App() {
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/my-listing' element={<MyListing />} />
-          <Route path='/about' element={<About />} />
+          
           <Route path='/profile' element={<Profile />} />
           <Route path='/update-listing/:listingId'element={<UpdateListing />}/> 
           <Route path='/create-listing' element={<CreateListing />} />
